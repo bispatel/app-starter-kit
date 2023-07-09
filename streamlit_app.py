b96172fbda64ade89d1a6cd629545e9e7fd60165
Bishwajit Patel <18615077+bispatel@users.noreply.github.com>
2023-07-09 23:24:59 +0530

    Excel Analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,28 @@ from langchain.document_loaders import CSVLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
 import os
-import pandas as pd
+import shutil
+
+# Specify the destination folder
+destination_folder = 'files'
 
 st.title('Bishwajit Search App')
 uploaded_file = st.file_uploader("Upload a file")
 
 if uploaded_file is not None:
     # Process the uploaded file
-    file_contents = uploaded_file.read()
+    # Save the uploaded file to the destination folder
     file_name = uploaded_file.name
+    file_path = os.path.join(destination_folder, file_name)
+    with open(file_path, "wb") as file:
+        shutil.copyfileobj(uploaded_file, file)
+    st.write("File uploaded successfully!")
     
+    #file_contents = uploaded_file.read()
     # Perform desired operations with the file
-    st.write("File Name:", file_name)
-    st.write("File Contents:")
-    st.write(file_contents)
+    #st.write("File Name:", file_name)
+    #st.write("File Contents:")
+    #st.write(file_contents)
     
 openai_api_key=st.sidebar.text_input('OpenAI API Key')
 
@@ -36,15 +44,18 @@ def generate_response(input_text):
   ##st.info(llm(input_text))
   os.environ["OPENAI_API_KEY"] = openai_api_key
   ##loader = CSVLoader(file_path='pokemon.csv')
-  folder_path = 'files'
+  #folder_path = 'files'
   ##loader = CSVLoader(folder_path=folder_path)
   print("Listing files")
-  dataframes = load_files(folder_path)
+  dataframes = load_files(destination_folder)
   print(dataframes)
+  # Create data loaders for each CSV file
+  data_loaders = [CSVLoader(filename) for filename in dataframes]
+
   #llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   #st.info(llm(input_text))
   index_creator = VectorstoreIndexCreator()
-  docsearch = index_creator.from_loaders(dataframes)
+  docsearch = index_creator.from_loaders(data_loaders)
   chain = RetrievalQA.from_chain_type(llm=OpenAI(),chain_type="stuff",retriever=docsearch.vectorstore.as_retriever(),input_key="question")
   response=chain({"question":input_text})
   st.info(response['result'])
@@ -57,3 +68,5 @@ with st.form('my_form'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
+    
+    
