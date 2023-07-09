d39ccc1ecb17b845060fe26bde6c40decb15fc74
Bishwajit Patel <18615077+bispatel@users.noreply.github.com>
2023-07-09 19:11:00 +0530

    Excel Analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,26 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.document_loaders import CSVLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import RetrievalQA
+import os
 
-st.title('🦜🔗 Quickstart App')
+st.title('Bishwajit Search App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+  #st.info(llm(input_text))
+  loader=CSVLoader(file_path='pokemon.csv')
+  index_creator = VectorstoreIndexCreator()
+  docsearch = index_creator.from_loaders([loader])
+  response=chain({"question":input_text})
+  st.info(response['result'])
+  print(response['result'])
+
+ 
+chain=RetrievalQA.from_chain_type(llm=OpenAI(),chain_type="stuff",retriever=docsearch.vectorstore.as_retriever(),input_key="question")
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
