160fbb1c8391a60b2badffe4656339debc30e4ba
Bishwajit Patel <18615077+bispatel@users.noreply.github.com>
2023-07-09 20:33:08 +0530

    Excel Analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ import os
 
 st.title('Bishwajit Search App')
 
-##openai_api_key = st.sidebar.text_input('OpenAI API Key')
-os.environ['OPENAI_API_KEY']= "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"
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+#os.environ['OPENAI_API_KEY']= "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"
 
 def generate_response(input_text):
   #llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -17,7 +17,7 @@ def generate_response(input_text):
   loader = CSVLoader(file_path='pokemon.csv')
   index_creator = VectorstoreIndexCreator()
   docsearch = index_creator.from_loaders([loader])
-  chain = RetrievalQA.from_chain_type(llm=OpenAI(),chain_type="stuff",retriever=docsearch.vectorstore.as_retriever(),input_key="question")
+  chain = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0.7, openai_api_key=openai_api_key),chain_type="stuff",retriever=docsearch.vectorstore.as_retriever(),input_key="question")
   response=chain({"question":input_text})
   st.info(response['result'])
   print(response['result'])
@@ -25,7 +25,7 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
- # if not openai_api_key.startswith('sk-'):
- #   st.warning('Please enter your OpenAI API key!', icon='⚠')
- # if submitted and openai_api_key.startswith('sk-'):
-  generate_response(text)
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
